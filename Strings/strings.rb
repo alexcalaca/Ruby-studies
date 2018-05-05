ea232bb3266074cae17942ae9d327f55ccb091ec
Alex Calaca <alexcalaca@gmail.com>
2018-05-05 00:48:23 +0000

    Add main file for Strings in Ruby

---
+++ Strings/strings.rb
@@ -0,0 +1,12 @@
+#A String object holds and manipulates an arbitrary sequence of bytes, typically representing characters. String objects may be created using String::new or as literals.
+
+
+puts "Your name:  "
+name = gets.chomp
+
+puts "Your age: "
+age = gets.chomp
+
+puts "Your name is #{name} and your age is #{age}"
+puts 'Your name is #{name} and your age is #{age'
+puts "#{'You are happy' if age.to_i == 24}"
